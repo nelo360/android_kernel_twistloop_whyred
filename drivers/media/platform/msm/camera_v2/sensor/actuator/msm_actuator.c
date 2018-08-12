d609b0955d6654869bad090c2e8eefa1db6e8a1f
Yaroslav Furman <yaro330@gmail.com>
2018-08-13 02:10:27 +0300

    msm_actuator: boost cpu and devfreq when focusing
    A small boost is never a bad thing.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Atman Shah <masteratman@gmail.com>
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -16,6 +16,8 @@
 #include "msm_sd.h"
 #include "msm_actuator.h"
 #include "msm_cci.h"
+#include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
 
 DEFINE_MSM_MUTEX(msm_actuator_mutex);
 
@@ -587,6 +589,9 @@ static int32_t msm_actuator_move_focus(
 	struct msm_camera_i2c_reg_setting reg_setting;
 
 	CDBG("called, dir %d, num_steps %d\n", dir, num_steps);
+
+	cpu_input_boost_kick_max(150);
+	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 200);
 
 	if (a_ctrl->step_position_table == NULL) {
 		pr_err("Step Position Table is NULL\n");
